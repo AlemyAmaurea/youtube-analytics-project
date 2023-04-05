ced5bc39c03bbc218308aa388f2d03bd0d13fa83
Andrey Vlasov <troller98@mail.ru>
2023-04-05 17:06:16 +0300

    Реализуйте класс Channel в файле src/channel.py Реализуйте метод print_info() класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
